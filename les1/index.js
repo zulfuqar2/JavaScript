7a3443fe342a4f546c9d2c3adcb6cda00bde2173
zulfuqar2 <anarzulfuqar2005@gmail.com>
2023-08-08 09:18:39 +0400

    try problem solving another way

---
+++ les1/index.js
@@ -0,0 +1,22 @@
+// logGreeting() adında funksiya tərtib edin hansıki konsola «[Adınız] is a JS developer» mesajı çıxar
+// function logGreeting(){
+//     const name = prompt("please enter your name ")
+//     console.log(`${name} is a JS developer` )
+// }
+// logGreeting();\
+
+
+//İstifadəçi adını parametr kimi qəbul edən və konsola «[İstifadəçi adı] is active now» mesajı çıxaran funksiya tərtib edin.
+//Funksiyanı 3 müxtəlif ad üçün çağırın.
+// let name = 0;
+// for (let i = 0; i < 3; i++) { // Changed the loop conditions to loop 3 times
+//     if (i <= 3) { // This condition is redundant, as 'i' will always be less than 3 in this loop
+//         logGreeting(); // Calling the function without passing 'name' as a parameter
+//     }
+// }
+
+// function logGreeting() {
+//     const name = prompt("Enter your name");
+//     console.log(`${name} is active now`);
+// }
+
